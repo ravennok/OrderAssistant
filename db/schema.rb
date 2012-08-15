19cd171f98eab9fb0c242b938618a6662179a7d9
Teemu Karvonen <ravennok@gmail.com>
2012-08-15 18:29:44 +0300

    Rollback from previos db-migration

--- db/schema.rb
+++ db/schema.rb
@@ -1,3 +1,4 @@
+# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -10,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120812100900) do
+ActiveRecord::Schema.define(:version => 20120809111703) do
 
   create_table "menus", :force => true do |t|
     t.string   "name"
@@ -20,14 +21,5 @@ ActiveRecord::Schema.define(:version => 20120812100900) do
     t.datetime "updated_at",  :null => false
     t.string   "picture_url"
   end
-
-  create_table "orders", :force => true do |t|
-    t.text     "body"
-    t.integer  "menu_id"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
-  end
-
-  add_index "orders", ["menu_id"], :name => "index_orders_on_menu_id"
 
 end
